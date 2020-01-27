1c418f164dd3e58124973a377701f1d13118cef1
TheNpDen4ik <npden4ik2@gmail.com>
2020-01-27 23:20:48 +0600

    git init

---
+++ Extentions/CryptorEngine.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace TestingSystem.Extentions
+{
+    public class CryptorEngine
+    {
+        public static string Encrypt(string salt, string toEncrypt)
+        {
+            var toEncryptArray = Encoding.UTF8.GetBytes(toEncrypt);
+            var hashed5 = new MD5CryptoServiceProvider();
+            var keyArray = hashed5.ComputeHash(Encoding.UTF8.GetBytes(salt));
+            hashed5.Clear();
+            var tades = new TripleDESCryptoServiceProvider
+            {
+                Key = keyArray, Mode = CipherMode.ECB, Padding = PaddingMode.PKCS7
+            };
+            var cTransform = tades.CreateEncryptor();
+            var resultArray = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+            tades.Clear();
+            return Convert.ToBase64String(resultArray, 0, resultArray.Length);
+        }
+
+
+        public static string Decrypt(string salt, string cipherString)
+        {
+            var toEncryptArray = Convert.FromBase64String(cipherString);
+            var hashed5 = new MD5CryptoServiceProvider();
+            var keyArray = hashed5.ComputeHash(Encoding.UTF8.GetBytes(salt));
+            var tades = new TripleDESCryptoServiceProvider
+            {
+                Key = keyArray, Mode = CipherMode.ECB, Padding = PaddingMode.PKCS7
+            };
+            var cTransform = tades.CreateDecryptor();
+            var resultArray = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+            tades.Clear();
+            return Encoding.UTF8.GetString(resultArray);
+        }
+
+    }
+}
---
+++ Models/Database/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TestingSystem.Models.Database
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options){}
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
